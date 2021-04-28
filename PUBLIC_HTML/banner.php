0fa6daed4fe1d30edc9f90125dd3f4c564e5ecac
Zak <46013015+Zymbol@users.noreply.github.com>
2021-04-28 11:09:21 -0700

    changed images

--- PUBLIC_HTML/banner.php
+++ PUBLIC_HTML/banner.php
@@ -1,7 +1,25 @@
-<div style="float: left;margin-left:-39px;" id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div style="float: left;margin-left:-39px;" id="carouselExampleControls" class="carousel slide" data-ride="carousel"
+  data-interval="10000">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="./images/banner.jpg" alt="First slide">
+    <div class="carousel-item active" >
+      <img class="d-block w-100" src="./images/banner.jpg" alt="Bently Cockpit">
     </div>
-  </div>  
+    <div style="float: left; width: 100%" class="carousel-item">
+      <img class="d-block w-100" src="./images/banner2.jpg" alt="Aerial View of Car">
+    </div>
+    <div style="float: left; width: 100%" class="carousel-item">
+      <img class="d-block w-100" src="./images/banner3.jpg" alt="Aerial View of Car">
+    </div>
+    <div style="float: left; width: 100%" class="carousel-item">
+      <img class="d-block w-100" src="./images/banner4.jpg" alt="Aerial View of Car">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>  
 </div>
