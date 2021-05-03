954a6e7486479440a8a0c837b5f38a337ac0d635
Zak <46013015+Zymbol@users.noreply.github.com>
2021-05-02 20:32:08 -0700

    minor fixes

--- PUBLIC_HTML/connect.php
+++ PUBLIC_HTML/connect.php
@@ -1,7 +1,7 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();    
-}
+// if (session_status() == PHP_SESSION_NONE) {
+//     session_start();    
+// }
 // Set up MySQL connection
 $host = 'localhost';
 $user = 'zscholefield';
